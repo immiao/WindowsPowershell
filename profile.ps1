b606b944b760adcd4598b94df53b6d2cf8a5f6b3
immiao <miaokaixiang@gmail.com>
2018-10-06 19:05:36 -0700

    Update

--- profile.ps1
+++ profile.ps1
@@ -1,3 +1,12 @@
+$rootPath = "D:\kamia"
+
+# This requires $rootPath\nuget.exe exists. If not, do the below scripts first.
+# $sourceNugetExe = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
+# $targetNugetExe = "D:\kamia\nuget.exe"
+# Invoke-WebRequest $sourceNugetExe -OutFile $targetNugetExe
+$targetNugetExe = "$rootPath\nuget.exe"
+Set-Alias nuget $targetNugetExe -Scope Global -Verbose
+
 function emacs($File) {
     $File = $File -replace "\\", "/"
     bash -c "emacs $File"
